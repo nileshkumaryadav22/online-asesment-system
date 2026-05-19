8c1378cc517e494c49526518113198f801a0ea65
Nilesh <nilesh@example.com>
2026-05-19 11:54:46 +0530

    Clean up scratch files

--- scratch_piston.js
+++
@@ -1,6 +0,0 @@
-fetch('https://emkc.org/api/v2/piston/runtimes')
-  .then(res => res.json())
-  .then(data => {
-    const supported = ['javascript', 'python', 'java', 'c++', 'cpp'];
-    console.log(data.filter(r => supported.includes(r.language)));
-  });
